577ec3db3e4e88623f36a1bc328125039f2d95e2
mtondolo <mtondolo@gmail.com>
2019-01-09 11:36:30 +0200

    Add get request

--- server.js
+++ server.js
@@ -1,16 +1,55 @@
 'use strict';
 
-const http = require('http');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+let contacts = require('./data');
+
+app.get('/api/contacts', (request, response) => {
+    if (!contacts) {
+        response.status(404).json({ message: 'No contacts found.' });
+      }
+   response.json(contacts);
+});
+
+app.get('/api/contacts/:id', (request, response) => {
+
+    let contactId = request.params.id;
+  
+    let contact = contacts.filter(contact => {
+      return contact.id == contactId;
+    });
+  
+    if (!contact) {
+      response.status(404).json({ message: 'Contact not found' });
+    }
+  
+    response.json(contact[0]);
+  });
+
+  app.post('/api/contacts', (request, response) => {
+
+    let contact = {
+      id: contacts.length + 1,
+      first_name: request.body.first_name,
+      last_name: request.body.last_name,
+      email: request.body.email,
+      website: request.body.website
+    };
+  
+    contacts.push(contact);
+  
+    response.json(contact);
+  
+  });
 
 const hostname = 'localhost';
 const port = 3001;
 
-const server = http.createServer((request, response) => {
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Hello world!\n');
-});
-
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
     console.log('Server is running at http://${hostname}:${port}');
 });
